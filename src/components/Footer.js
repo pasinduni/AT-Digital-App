8dcadc54dcd6e113811d114b463c7c66c29fb1a0
pasiduni <pasidunishanika@gmail.com>
2024-04-12 16:42:08 +0530

    updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,34 +1,44 @@
-import React from 'react'
+import React from "react";
 import Logo from "../Assets/Logo.png";
 
 const Footer = () => {
-    return (
-        <div className='footer-wrapper'>
-            <div className="footer-section-one">
-                <div className="footer-logo-container">
-                    <img src={Logo} alt="" />
-                    <p>Your goal is our target. Not anything in between. We use online marketing platforms and tools to achieve single objective - your business results.</p>
-                </div>
-            </div>
-    
-            <div className="footer-section-two">
-                <div className="footer-section-columns">
-                    <span className='header'>Our Technologies</span>
-                    <span>ReactJS</span>
-                    <span>Gatsby</span>
-                    <span>NextJS</span>
-                    <span>NodeJS</span>
-                </div>
-                <div className="footer-section-columns">
-                    <span className='header'>Our Services</span>
-                    <span>Social Media Marketing</span>
-                    <span>Web % Mobile App Development</span>
-                    <span>Data & Analytics</span>
-                    <span>NodeJS</span>
-                </div>
-            </div>
+  return (
+    <div className="footer-wrapper">
+      <div className="footer-container">
+        <div className="footer-section-one">
+          <div className="footer-logo-container">
+            <img src={Logo} alt="" />
+            <p>
+              Your goal is our target. Not anything in between. We use online
+              marketing platforms and tools to achieve single objective - your
+              business results.
+            </p>
+          </div>
         </div>
-      )
-    }
 
-export default Footer
+        <div className="footer-section-two">
+          <div className="footer-section-columns">
+            <span className="header">Our Technologies</span>
+            <span>ReactJS</span>
+            <span>Gatsby</span>
+            <span>NextJS</span>
+            <span>NodeJS</span>
+          </div>
+          <div className="footer-section-columns">
+            <span className="header">Our Services</span>
+            <span>Social Media Marketing</span>
+            <span>Web % Mobile App Development</span>
+            <span>Data & Analytics</span>
+            <span>NodeJS</span>
+          </div>
+        </div>
+      </div>
+      <div className="footer-section-three">
+        <hr />
+        <span>Privacy Policy | Terms & Conditions</span>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,41 +1,40 @@
-import React from "react";
-import Logo from "../Assets/Logo.png"
-import { useState } from "react";
+import React, { useState } from "react";
+import Logo from "../Assets/Logo.png";
 import { HiOutlineBars3 } from "react-icons/hi2";
+import { HiX } from "react-icons/hi";
 
 const Navbar = () => {
   const [openMenu, setOpenMenu] = useState(false);
-  const menuOptions = [
-    {
-      text: "Services",
-    },
-    {
-      text: "About Us",
-    },
-    {
-      text: "Contact Us",
-    },
-    {
-      text: "Careers",
-    },
-  ];
+
+  const toggleMenu = () => {
+    setOpenMenu(!openMenu);
+  };
 
   return (
     <nav>
-      <div className="navbar-logo">
-        <img src={Logo} alt="" />
-      </div>
-      <div className="navbar-links">
+    <div className="navbar-logo">
+      <img src={Logo} alt="" />
+    </div>
+    <div className="navbar-links">
+      <a href="">Services</a>
+      <a href="">About Us</a>
+      <a href="">Contact Us</a>
+      <a href="">Careers</a>
+    </div>
+    <div className="navbar-menu-icon" onClick={toggleMenu}>
+      {openMenu ? <HiX /> : <HiOutlineBars3 />}
+    </div>
+    {openMenu && (
+      <div className="navbar-menu">
+        <a href="">Home</a>
         <a href="">Services</a>
         <a href="">About Us</a>
         <a href="">Contact Us</a>
         <a href="">Careers</a>
       </div>
-      <div className="navbar-menu">
-        <HiOutlineBars3 onClick={() => setOpenMenu(true)} />
-      </div>
-    </nav>
-  );
+    )}
+  </nav>
+);
 };
 
 export default Navbar;
